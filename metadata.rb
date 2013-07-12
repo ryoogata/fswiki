c6cb26451bafcf420c4833b7f6a652b11456c187
root <root@ip-10-196-87-4.us-west-1.compute.internal>
2013-07-12 11:28:27 +0000

    metadata.rb の編集とmetadata.json の作成

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'wiki'
+name             'fswiki'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
 license          'All rights reserved'
 description      'Installs/Configures wiki'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends "apache2"
+depends          "apache2"
